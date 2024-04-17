1e9d4eaf042b711b5eb3079912b79f1efdb8ef0a
joserafael0160 <joserafaelperezrivero113@gmail.com>
2024-04-17 19:47:11 -0400

    Adding functionalities

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-const time = document.querySelector(".time")
+"use strict";
+const time = document.querySelector(".time");
+
+const convert = n => n.toString().padStart(2,"0");
+
+function getCurrentTime() {
+  const date = new Date;
+  const currentTime = convert(date.getHours()) + ":" + convert(date.getMinutes()) + ":" + convert(date.getSeconds());
+  time.innerHTML = currentTime; 
+  time.dateTime = currentTime;
+}
+
+setInterval(getCurrentTime,1000); 
